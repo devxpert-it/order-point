cd05b4e2bc31b013d85718ff38d5287ebc8c0474
mucnjakf <crofilix@gmail.com>
2026-04-29 00:23:25 +0200

    Add database migration application and endpoint mapping extensions

---
+++ src/OrderPoint.Api/Extensions/EndpointExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace OrderPoint.Api.Extensions;
+
+internal static class EndpointExtensions
+{
+    internal static IServiceCollection AddEndpoints(this IServiceCollection services, Assembly assembly)
+    {
+        ServiceDescriptor[] serviceDescriptors = assembly
+            .DefinedTypes
+            .Where(type =>
+                type is { IsAbstract: false, IsInterface: false } &&
+                type.IsAssignableTo(typeof(IEndpoint)))
+            .Select(type => ServiceDescriptor.Transient(typeof(IEndpoint), type))
+            .ToArray();
+
+        services.TryAddEnumerable(serviceDescriptors);
+
+        return services;
+    }
+
+    internal static IApplicationBuilder MapEndpoints(this WebApplication app)
+    {
+        var endpoints = app.Services.GetRequiredService<IEnumerable<IEndpoint>>();
+
+        foreach (IEndpoint endpoint in endpoints)
+        {
+            endpoint.MapEndpoint(app);
+        }
+
+        return app;
+    }
+}
+
+internal interface IEndpoint
+{
+    void MapEndpoint(IEndpointRouteBuilder app);
+}
--- src/OrderPoint.Api/Program.cs
+++ src/OrderPoint.Api/Program.cs
@@ -1,3 +1,7 @@
+using System.Reflection;
+using FluentValidation;
+using OrderPoint.Api.Exceptions;
+using OrderPoint.Api.Extensions;
 using OrderPoint.Application;
 using OrderPoint.Infrastructure;
 using OrderPoint.ServiceDefaults;
@@ -10,19 +14,39 @@ builder.AddServiceDefaults();
 // API docs
 builder.Services.AddOpenApi();
 
+// Error handling
+builder.Services.AddProblemDetails();
+builder.Services.AddExceptionHandler<RequestValidationExceptionHandler>();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
+// Minimal API
+builder.Services.AddEndpoints(Assembly.GetExecutingAssembly());
+
+// FluentValidation
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+
 // Modules
-builder.Services.AddApplicationModule();
-builder.Services.AddInfrastructureModule();
+builder.Services.AddApplicationModule(builder.Configuration);
+builder.Services.AddInfrastructureModule(builder.Configuration);
 
 // ---------------------------------------------------------------------------------------------------------------------
 
 WebApplication app = builder.Build();
 
+// Minimal API
+app.MapEndpoints();
+
 // API docs
 app.MapOpenApi();
 
+// Database
+app.ApplyMigrations();
+
 // HTTP
 app.UseHttpsRedirection();
+
+// Error handling
+app.UseExceptionHandler();
 
 // Aspire
 app.MapDefaultEndpoints();
