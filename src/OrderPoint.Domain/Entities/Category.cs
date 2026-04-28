edfa383c64cdc35dcdb3e958b369662357ec654e
mucnjakf <crofilix@gmail.com>
2026-04-29 00:20:49 +0200

    Add base Entity class and Category entity

---
+++ src/OrderPoint.Domain/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using OrderPoint.Domain.Entities.Base;
+
+namespace OrderPoint.Domain.Entities;
+
+public sealed class Category : Entity
+{
+    public string Name { get; private set; }
+
+    private Category(
+        Guid id,
+        string name,
+        DateTimeOffset createdAtUtc) : base(id, createdAtUtc) => Name = name;
+}
