cda5c8aa2331d8274cadce8ad171d3e7fca0e0bf
mucnjakf <crofilix@gmail.com>
2026-05-01 04:02:58 +0200

    Enhance CategoriesTableSkeleton with table structure and multiple skeleton rows

--- client/admin-web/src/pages/category/components/CategoriesTableSkeleton.jsx
+++ client/admin-web/src/pages/category/components/CategoriesTableSkeleton.jsx
@@ -1,10 +1,55 @@
-import { Paper, Skeleton } from "@mui/material";
+import {
+  Paper,
+  Skeleton,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 
 function CategoriesTableSkeleton() {
   return (
-    <Paper>
-      <Skeleton variant="rounded" height={400} />
-    </Paper>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {Array.from({ length: 5 }).map((_, i) => (
+              <TableCell key={i}>
+                <Skeleton variant="rounded" height={16} />
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {Array.from({ length: 5 }).map((_, rowIndex) => (
+            <TableRow key={rowIndex}>
+              <TableCell>
+                <Skeleton variant="rounded" height={40} width={40} />
+              </TableCell>
+              <TableCell>
+                <Skeleton variant="rounded" height={16} width={100} />
+              </TableCell>
+              <TableCell>
+                <Skeleton variant="rounded" height={16} width={100} />
+              </TableCell>
+              <TableCell>
+                <Skeleton variant="rounded" height={16} width={100} />
+              </TableCell>
+              <TableCell align="right">
+                <Skeleton
+                  variant="rounded"
+                  height={16}
+                  width={100}
+                  sx={{ ml: "auto" }}
+                />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
